dd633e68d15054219cdc800d4b1dd6848d154493
steveinderau <114860199+steveinderau@users.noreply.github.com>
2022-12-16 14:34:38 -0800

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,31 +6,18 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     Pause = 1
     basic.setLedColor(0xff0000)
 })
-input.onGesture(Gesture.ScreenUp, function () {
-    basic.showLeds(`
-        . . . # #
-        . . . # #
-        # # # # #
-        # # # # #
-        . # . # .
-        `)
-    if (Pause == 0) {
-        basic.setLedColor(0x00ff00)
-    } else {
-        basic.setLedColor(0xff0000)
-    }
-})
-let Bright = 0
 let Pause = 0
 basic.setLedColor(0x00ff00)
 Pause = 0
 let Steps = 5
 let Speed = 50
+let Bright = 0
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     if (Pause == 0) {
         if (Bright >= 255) {
             Steps = -10
-        } else if (Bright <= 0) {
+        } else if (Bright <= 5) {
             Steps = 10
         }
         Bright = Steps + Bright
